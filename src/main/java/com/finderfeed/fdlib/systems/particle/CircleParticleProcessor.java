33e5afabb92ad955c524a599f6e9d7900415aa00
FINDERFEED <windingaster335@gmail.com>
2024-10-21 19:14:37 +0300

    untested circle processor

--- src/main/java/com/finderfeed/fdlib/systems/particle/CircleParticleProcessor.java
+++ src/main/java/com/finderfeed/fdlib/systems/particle/CircleParticleProcessor.java
@@ -3,6 +3,7 @@ package com.finderfeed.fdlib.systems.particle;
 import com.finderfeed.fdlib.FDLib;
 import com.finderfeed.fdlib.util.FDByteBufCodecs;
 import com.finderfeed.fdlib.util.FDCodecs;
+import com.finderfeed.fdlib.util.math.FDMathUtil;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.client.particle.Particle;
@@ -10,15 +11,20 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
+import org.joml.*;
 
 public class CircleParticleProcessor implements ParticleProcessor<CircleParticleProcessor> {
+
 
     private Vec3 point;
     private boolean forward;
     private int circleCount;
 
     private Vec3 initialPoint = null;
+    private Vector3f axis = null;
+    private Vec3 oldSpeed = Vec3.ZERO;
 
     public CircleParticleProcessor(Vec3 point,boolean forward,int circleCount){
         this.point = point;
@@ -35,13 +41,52 @@ public class CircleParticleProcessor implements ParticleProcessor<CircleParticle
     public void processParticle(Particle particle) {
         if (initialPoint == null){
             initialPoint = new Vec3(particle.x,particle.y,particle.z);
+            Vec3 b = initialPoint.subtract(point);
+            Vec3 left = b.cross(new Vec3(0,1,0));
+            Vec3 axis = left.cross(b);
+            this.axis = new Vector3f((float)axis.x,(float)axis.y,(float)axis.z);
+
         }
+        if (particle.age > particle.lifetime) return;
 
-        float p = particle.age / (float) particle.lifetime;
+        float p2 = particle.age / (float) particle.lifetime;
 
+        double len = initialPoint.subtract(point).length();
 
+        double targetRad = p2 * len;
 
+        float fullAngle = FDMathUtil.FPI * 2 * circleCount;
 
+        float targetAngle = fullAngle * p2;
+
+        if (!forward) targetAngle = -targetAngle;
+
+        Quaternionf q = new Quaternionf(new AxisAngle4f(targetAngle,axis.x,axis.y,axis.z));
+
+        var p = q.transform(
+                (initialPoint.x - point.x) / len,
+                (initialPoint.y - point.y) / len,
+                (initialPoint.z - point.z) / len,
+                new Vector3d()
+        );
+
+        Vec3 targetPoint = point.add(
+                p.x * targetRad,
+                p.y * targetRad,
+                p.z * targetRad
+        );
+
+        Vec3 speed = new Vec3(particle.x,particle.y,particle.z).subtract(targetPoint);
+
+        particle.xd -= oldSpeed.x / (particle.friction);
+        particle.yd -= oldSpeed.y / (particle.friction);
+        particle.zd -= oldSpeed.z / (particle.friction);
+
+        particle.xd += speed.x;
+        particle.yd += speed.y;
+        particle.zd += speed.z;
+
+        oldSpeed = speed;
     }
 
 
