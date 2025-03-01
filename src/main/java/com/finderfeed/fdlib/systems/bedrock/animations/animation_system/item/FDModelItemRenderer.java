9bc7928ce1dcf593944842dddafbb4076872ac94
FINDERFEED <windingaster335@gmail.com>
2025-03-01 19:20:43 +0300

    item renderer

---
+++ src/main/java/com/finderfeed/fdlib/systems/bedrock/animations/animation_system/item/FDModelItemRenderer.java
@@ -0,0 +1,85 @@
+package com.finderfeed.fdlib.systems.bedrock.animations.animation_system.item;
+
+import com.finderfeed.fdlib.systems.bedrock.models.FDModel;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import org.joml.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FDModelItemRenderer extends BlockEntityWithoutLevelRenderer {
+
+    public List<FDModel> models;
+
+    public FDModelItemRendererOptions options;
+
+    public FDModelItemRenderer(FDModelItemRendererOptions options) {
+        super(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
+        this.options = options;
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack matrices, MultiBufferSource buffer, int packedLight, int packedOverlay) {
+        this.initModelsIfNecessary();
+        float scale = options.scale.apply(displayContext);
+
+        if (displayContext == ItemDisplayContext.GUI){
+            Vector3f translation = options.translation.apply(displayContext);
+            matrices.translate(0.5 + translation.x, 0.25f + translation.y, 0.5 + translation.z);
+            matrices.mulPose(Axis.XP.rotationDegrees(35));
+            matrices.mulPose(Axis.YP.rotationDegrees(45 + options.rotation.apply(displayContext)));
+        }else {
+            Vector3f translation = options.translation.apply(displayContext);
+            if (displayContext == ItemDisplayContext.GROUND) {
+                matrices.translate(0.5 + translation.x, 0.3 + translation.y, 0.5 + translation.z);
+            }else{
+                matrices.translate(0.5 + translation.z, 0.5 + translation.y, 0.5 + translation.x);
+            }
+            matrices.mulPose(Axis.YP.rotationDegrees(options.rotation.apply(displayContext)));
+        }
+        matrices.scale(scale,scale,scale);
+
+        for (int i = 0; i < models.size();i++){
+            FDModel model = models.get(i);
+
+            VertexConsumer vertex = buffer.getBuffer(options.renderTypes.get(i));
+
+            model.render(matrices,vertex,packedLight,OverlayTexture.NO_OVERLAY,1f,1f,1f,1f);
+        }
+    }
+
+    private void initModelsIfNecessary(){
+        if (models == null){
+            models = new ArrayList<>();
+            for (var sup : this.options.modelInfos){
+                models.add(new FDModel(sup.get()));
+            }
+        }
+    }
+
+
+    public static IClientItemExtensions createExtensions(FDModelItemRendererOptions options){
+        return new IClientItemExtensions() {
+
+            private FDModelItemRenderer renderer = new FDModelItemRenderer(options);
+
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return renderer;
+            }
+
+        };
+    }
+
+}
+
+
---
+++ src/main/java/com/finderfeed/fdlib/systems/bedrock/animations/animation_system/item/FDModelItemRendererOptions.java
@@ -0,0 +1,67 @@
+package com.finderfeed.fdlib.systems.bedrock.animations.animation_system.item;
+
+import com.finderfeed.fdlib.systems.bedrock.models.FDModelInfo;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.item.ItemDisplayContext;
+import org.joml.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class FDModelItemRendererOptions {
+
+    protected List<Supplier<FDModelInfo>> modelInfos = new ArrayList<>();
+    protected List<RenderType> renderTypes = new ArrayList<>();
+    protected Function<ItemDisplayContext, Float> rotation = t -> {
+        return 0f;
+    };
+    protected Function<ItemDisplayContext, Float> scale = (t)->{
+        return t == ItemDisplayContext.GUI ? 0.6f : 0.35f;
+    };
+    protected Function<ItemDisplayContext, Vector3f> translation = t -> {
+        return new Vector3f();
+    };
+
+    public FDModelItemRendererOptions addModel(Supplier<FDModelInfo> info, RenderType renderType){
+        modelInfos.add(info);
+        renderTypes.add(renderType);
+        return this;
+    }
+
+    public FDModelItemRendererOptions addRotation(Function<ItemDisplayContext, Float> yRotation){
+        this.rotation = yRotation;
+        return this;
+    }
+
+    public FDModelItemRendererOptions setScale(Function<ItemDisplayContext, Float> scaling){
+        this.scale = scaling;
+        return this;
+    }
+
+    public FDModelItemRendererOptions addTranslation(Function<ItemDisplayContext, Vector3f> translation){
+        this.translation = translation;
+        return this;
+    }
+
+    public FDModelItemRendererOptions addRotation(float rotation){
+        this.rotation = t->rotation;
+        return this;
+    }
+
+    public FDModelItemRendererOptions setScale(float scale){
+        this.scale = t->scale;
+        return this;
+    }
+
+    public FDModelItemRendererOptions addTranslation(Vector3f translation){
+        this.translation = (t)->translation;
+        return this;
+    }
+
+    public static FDModelItemRendererOptions create(){
+        return new FDModelItemRendererOptions();
+    }
+
+}
