4cbcd9485e6309b9dab4a7e88ed71b8ccf7c69e9
FINDERFEED <windingaster335@gmail.com>
2025-05-03 21:00:53 +0300

    client camera pulls effects from player

--- src/main/java/com/finderfeed/fdlib/FDClientHelpers.java
+++ src/main/java/com/finderfeed/fdlib/FDClientHelpers.java
@@ -1,12 +1,17 @@
 package com.finderfeed.fdlib;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class FDClientHelpers {
 
     public static Level getClientLevel(){
         return Minecraft.getInstance().level;
+    }
+
+    public static Player getClientPlayer(){
+        return Minecraft.getInstance().player;
     }
 
 }
--- src/main/java/com/finderfeed/fdlib/systems/cutscenes/ClientCameraEntity.java
+++ src/main/java/com/finderfeed/fdlib/systems/cutscenes/ClientCameraEntity.java
@@ -1,15 +1,22 @@
 package com.finderfeed.fdlib.systems.cutscenes;
 
+import com.finderfeed.fdlib.FDClientHelpers;
 import com.finderfeed.fdlib.init.FDEntities;
+import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
-public class ClientCameraEntity extends Entity {
+public class ClientCameraEntity extends LivingEntity {
 
-    public ClientCameraEntity(EntityType<?> p_19870_, Level p_19871_) {
+    public ClientCameraEntity(EntityType<? extends LivingEntity> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
     }
 
@@ -24,22 +31,71 @@ public class ClientCameraEntity extends Entity {
     }
 
     @Override
+    public HumanoidArm getMainArm() {
+        return HumanoidArm.LEFT;
+    }
+
+    @Override
     public void setPos(double p_20210_, double p_20211_, double p_20212_) {
         super.setPos(p_20210_, p_20211_, p_20212_);
     }
 
+
     @Override
-    protected void defineSynchedData(SynchedEntityData.Builder p_326003_) {
+    public boolean hasEffect(Holder<MobEffect> effect) {
+        return FDClientHelpers.getClientPlayer().hasEffect(effect);
+    }
+
+    @Nullable
+    @Override
+    public MobEffectInstance getEffect(Holder<MobEffect> effect) {
+        return FDClientHelpers.getClientPlayer().getEffect(effect);
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag p_20052_) {
 
     }
 
     @Override
-    protected void readAdditionalSaveData(CompoundTag p_20052_) {
+    public Iterable<ItemStack> getArmorSlots() {
+        return null;
+    }
+
+    @Override
+    public ItemStack getItemBySlot(EquipmentSlot p_21127_) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public void setItemSlot(EquipmentSlot p_21036_, ItemStack p_21037_) {
 
     }
 
     @Override
-    protected void addAdditionalSaveData(CompoundTag p_20139_) {
+    public void addAdditionalSaveData(CompoundTag p_20139_) {
 
     }
+
+    @Override
+    public void push(Vec3 p_347665_) {
+
+    }
+
+    @Override
+    public void push(Entity p_21294_) {
+
+    }
+
+    @Override
+    public void push(double p_20286_, double p_20287_, double p_20288_) {
+
+    }
+
+    @Override
+    protected void pushEntities() {
+
+    }
+
+
 }
